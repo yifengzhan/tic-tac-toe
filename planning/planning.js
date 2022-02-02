21bbf30b7f0c006fc0e64ceecdeb69fb7b613aac
Yifeng_Z <yifengzhan@gmail.com>
2022-02-02 11:32:34 +1100

    pseudo code for p1

---
+++ planning/planning.js
@@ -0,0 +1,24 @@
+Step 1: in html make 3 columns, 3 rows and set even gaps among them , in total 9 boxes display in grid
+
+
+
+
+Step 2 : in JavaScript
+palyer 1 clicks random box : check if that box been clicked?
+if it has been clicked/marked a cross or circle, no double clicking allowed
+if it has not been clicked, put current player's marking on the box 
+if there is an winning condition (win/loose /draw?)
+if any condition met, end the game, give a message " you have....the game"
+
+if winning conditon not met, change to palyer 2 
+repeat line 9-13 
+
+
+
+
+
+
+
+
+
+
